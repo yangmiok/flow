c99dee3af30cf52e5b8809d6776ef2557586fe60
mengmanzbh <1361006944@qq.com>
2019-02-12 17:38:40 +0800

    OK

--- flow/api/flow.go
+++ flow/api/flow.go
@@ -116,12 +116,61 @@ func FlowRecharge(ctx *gin.Context) {
 
 //订单状态查询
 func FlowOrdersta(ctx *gin.Context) {
+    orderid := ctx.PostForm("orderid")
 
+   //请求地址
+    juheURL :="http://v.juhe.cn/flow/ordersta"
+ 
+    //初始化参数
+    param:=url.Values{}
+ 
+    //配置请求参数,方法内部已处理urlencode问题,中文参数可以直接传参
+    param.Set("orderid",orderid) //商家订单号，8-32位字母数字组合
+    param.Set("key",APPKEY) //应用APPKEY(应用详细页查询)
+ 
+
+    //发送请求
+    data,err:=utils.Post(juheURL,param)
+    if err!=nil{
+        fmt.Errorf("请求失败,错误信息:\r\n%v",err)
+        ctx.JSON(404, gin.H{
+            "code": "404",
+            "message": err,
+            })
+    }else{
+        var netReturn map[string]interface{}
+        json.Unmarshal(data,&netReturn)
+
+        ctx.JSON(200, gin.H{
+            "error_code": netReturn["error_code"],
+            "message": netReturn["reason"],
+            "result":netReturn["result"],
+        })
+    }
 
 }
 
 //状态回调配置
 func Setpush(ctx *gin.Context) {
-
+   //请求地址
+    juheURL :="充值接口测试完毕，联系在线客服进行回调配置"
+ 
+    //初始化参数
+    param:=url.Values{}
+ 
+    //配置请求参数,方法内部已处理urlencode问题,中文参数可以直接传参
+ 
+ 
+    //发送请求
+    data,err:=utils.Post(juheURL,param)
+    if err!=nil{
+        fmt.Errorf("请求失败,错误信息:\r\n%v",err)
+    }else{
+        var netReturn map[string]interface{}
+        json.Unmarshal(data,&netReturn)
+        if netReturn["error_code"].(float64)==0{
+            fmt.Printf("接口返回result字段是:\r\n%v",netReturn["result"])
+        }
+    }
 
 }
